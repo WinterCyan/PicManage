9715a52db6b65a2aeedf5cf7e313c27d1188bacb
WinterCyan <wintercyan98@gmail.com>
2018-10-29 21:57:15 +0800

    hide passwd

--- src/db/DBInfo.java
+++ src/db/DBInfo.java
@@ -1,6 +1,6 @@
 package db;
 public class DBInfo {
     public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static final String DB_URL = "jdbc:mysql://localhost?allowPublicKeyRetrieval=true&useSSL=false&user=root&password=mysql.winter";
-    public static final String DB_DB_URL = "jdbc:mysql://localhost/picmanage?allowPublicKeyRetrieval=true&useSSL=false&user=root&password=mysql.winter";
+    public static final String DB_URL = "jdbc:mysql://localhost?allowPublicKeyRetrieval=true&useSSL=false&user=root&password=********";
+    public static final String DB_DB_URL = "jdbc:mysql://localhost/picmanage?allowPublicKeyRetrieval=true&useSSL=false&user=root&password=********";
 }
